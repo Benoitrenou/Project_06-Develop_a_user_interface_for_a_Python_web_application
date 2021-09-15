920b5f56b54c690f2a70078ac1a5d59be8a7deed
Benoitrenou <benoit.renou@gmail.com>
2021-09-14 21:02:15 -0300

    Update

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,39 @@
 let films = [];
 
-const test = async function (page, index, url) {
+let test = async function (url, index) {
   let response = await fetch(url);
   let data = await response.json();
-  let imageUrl = data.results[index].image_url;
-  
-  document.getElementById("movie"+page+index).setAttribute("src",imageUrl);
-  films.push(imageUrl);
-  return imageUrl;
+  let imageUrl = await data.results[index].image_url;
+  films.push({src: await imageUrl});
+  document.getElementById("movie10").setAttribute("src", imageUrl)
 }
-for (i=0 ; i<5 ; i++) {
-  test(1, i, 'http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score&page=1')
-  } 
-  for (i=0 ; i<3 ; i++) {
-    test(2, i, 'http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score&page=2')
-    }
+
+let url = 0;
+let test2 = function (url, index) {
+  fetch (url)
+  .then (function(res) {
+    if (res.ok) {return res.json();}
+  })
+  .then (function (value) {
+    console.log(value.results[index].image_url);
+    let url = value.results[index].image_url;
+    console.log(url);
+    return url
+  })
+  .then (function(url) {
+    document.getElementById("movie10").setAttribute("src", url)
+  })
+  .catch (function (err){
+    console.log(err)
+  })
+}
+
+for (i=0; i<5; i++) {
+  test ("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score", i);
+};
+for (i=0; i<2; i++){
+  test ("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score&page=2", i);
+};
 
 console.log(films);
 
@@ -40,7 +59,7 @@ var closeButton = document.getElementById("closeButton");
 let openModal = function (movie_id) {
     modal.style.display = null;
     let element = document.getElementById("BestMovie");
-    getMovieInfos (element.getAttribute('id_api'));
+//    getMovieInfos (element.getAttribute('id_api'));
 }
 
 // When the user clicks on (x), close the modal
@@ -72,6 +91,8 @@ let movies = [
   {src:"https://images.unsplash.com/photo-1528495612343-9ca9f4a4de28?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",},
   {src:"https://images.unsplash.com/photo-1518715303843-586e350765b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",},
 ];
+
+console.log(movies);
 
 // Fill the slider with all the movies in the "movies" array
 function populateSlider() {
@@ -113,7 +134,6 @@ btnLeft.addEventListener("click", (e) => {
     behavior: "smooth",
   });
   activeIndex = (activeIndex - 1) % 3;
-  console.log(activeIndex);
   updateIndicators(activeIndex);
 });
 
@@ -122,9 +142,6 @@ btnRight.addEventListener("click", (e) => {
   let movieWidth = document.querySelector(".movie").getBoundingClientRect()
     .width;
   let scrollDistance = movieWidth * 6; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
-
-  console.log(`movieWidth = ${movieWidth}`);
-  console.log(`scrolling right ${scrollDistance}`);
 
   // if we're on the last page
   if (activeIndex == 1) {
@@ -144,7 +161,6 @@ btnRight.addEventListener("click", (e) => {
       behavior: "smooth",
     });
     activeIndex = (activeIndex + 1) % 3;
-    console.log(activeIndex);
     updateIndicators(activeIndex);
   }
 });
