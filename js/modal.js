0000877e77100d00d31c2a9c39ff3a54ceec5ff2
Benoitrenou <benoit.renou@gmail.com>
2021-09-18 10:05:43 -0300

    Update
    CSS :
    Add Gotham police
    Refactoring logo
    
    HTML:
    Modify footer
    
    JS:
    Update slider's indicators functions

--- js/modal.js
+++ js/modal.js
@@ -15,15 +15,76 @@ let getMainMovie = function () {
 
 
 //SLIDER SECTION
-const slider = document.querySelector(".slider");
-const btnLeft = document.getElementById("moveLeft");
-const btnRight = document.getElementById("moveRight");
-const indicators = document.querySelectorAll(".indicator");
 
-let baseSliderWidth = slider.offsetWidth;
+//Define indicators for scrolling method
+const indicators0 = document.querySelectorAll(".indic0");
+const indicators1 = document.querySelectorAll(".indic1");
+const indicators2 = document.querySelectorAll(".indic2");
+const indicators3 = document.querySelectorAll(".indic3");
+
+
+//let baseSliderWidth = slider.offsetWidth;
 let activeIndex = 0; // the current page on the slider
 
-//let films = [];
+// Update the indicators that show which page we're currently on
+function updateIndicators(index, indicators) {
+  indicators.forEach((indicator) => {
+    indicator.classList.remove("active");
+  });
+  let newActiveIndicator = indicators[index];
+  newActiveIndicator.classList.add("active");
+}
+
+// Scroll Left button function
+const scrollLeft = function(slider, indicators) {
+  let movieWidth = document.querySelector(".movie").getBoundingClientRect()
+    .width;
+  let scrollDistance = movieWidth * 3; // Scroll the length of 6 movies
+  slider.scrollBy({
+    top: 0,
+    left: -scrollDistance,
+    behavior: "smooth",
+  });
+  switch(activeIndex) {
+    case 0:
+      activeIndex=1;
+      break;
+    case 1:
+      activeIndex=0;
+      break;
+    default:
+      activeIndex=0;
+  }
+  updateIndicators(activeIndex, indicators);
+}
+
+// Scroll Right button function
+const scrollRight = function(slider, sliderId, array, indicators) {
+  let movieWidth = document.querySelector(".movie").getBoundingClientRect()
+    .width;
+  let scrollDistance = movieWidth * 3; // Scroll the length of 6 movies
+
+  // if we're on the last page
+  if (activeIndex == 1) {
+    createSlide(sliderId, array);
+    slider.scrollBy({
+      top: 0,
+      left: +scrollDistance,
+      behavior: "smooth",
+    });
+    activeIndex = 0;
+    updateIndicators(activeIndex, indicators);
+  } else {
+    slider.scrollBy({
+      top: 0,
+      left: +scrollDistance,
+      behavior: "smooth",
+    });
+    activeIndex = (activeIndex + 1) % 3;
+    updateIndicators(activeIndex, indicators);
+  }
+}
+
 let getMoviesInfos = async function (url, index, array) {
   let response = await fetch(url);
   let data = await response.json();
@@ -72,98 +133,47 @@ let loadBestsSlider = async function () {
   await getMoviesInfos ("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score&page=2", 3, bestsArray);
   bestsArray.splice(0, 1);
   createSlide("bestMoviesSlider", bestsArray);
+  let btnLeft = document.getElementById("moveLeft0");
+  let btnRight = document.getElementById("moveRight0");
+  let slider = document.getElementById("bestMoviesSlider")
+  btnRight.addEventListener("click", (e) => {
+    scrollRight(slider, "bestMoviesSlider", bestsArray, indicators0);  
+  });
+  btnLeft.addEventListener("click", (e) => {
+    scrollLeft(slider, indicators0);
+  });
+  return true
 }
 
-let loadCategSlider = async function(slider, genre) {
+let loadCategSlider = async function(number, sliderid, genre, indicators) {
   let categArray = [];
   await getMoviesInfos ("http://127.0.0.1:8000/api/v1/titles/?genre="+genre+"&sort_by=-imdb_score", 5, categArray);
   await getMoviesInfos ("http://127.0.0.1:8000/api/v1/titles/?genre="+genre+"&sort_by=-imdb_score&page=2", 2, categArray);
-  createSlide(slider, categArray);
+  createSlide(sliderid, categArray);
+  let btnLeft = document.getElementById("moveLeft"+number);
+  let btnRight = document.getElementById("moveRight"+number);
+  let slider = document.getElementById(sliderid)
+  btnRight.addEventListener("click", (e) => {
+    scrollRight(slider, sliderid, categArray, indicators);  
+  });
+  btnLeft.addEventListener("click", (e) => {
+    scrollLeft(slider, indicators);
+  return true
+})
 }
 
 async function init() {
   getMainMovie();
   loadBestsSlider();
-  loadCategSlider("categ1Slider", "Western");
-  loadCategSlider("categ2Slider", "Fantasy");
-  loadCategSlider("categ3Slider", "Thriller");
+  loadCategSlider(1, "categ1Slider", "Western", indicators1);
+  loadCategSlider(2, "categ2Slider", "History", indicators2);
+  loadCategSlider(3, "categ3Slider", "Thriller", indicators3);
   return true;
 }
 
 document.getElementsByTagName("body").onload = init();
 
-// Update the indicators that show which page we're currently on
-function updateIndicators(index) {
-  indicators.forEach((indicator) => {
-    indicator.classList.remove("active");
-  });
-  console.log(indicators);
-  let newActiveIndicator = indicators[index];
-  newActiveIndicator.classList.add("active");
-}
-
-// Scroll Left button
-const scrollLeft = function() {
-  let movieWidth = document.querySelector(".movie").getBoundingClientRect()
-    .width;
-  let scrollDistance = movieWidth * 6; // Scroll the length of 6 movies
-  slider.scrollBy({
-    top: 0,
-    left: -scrollDistance,
-    behavior: "smooth",
-  });
-  //activeIndex = (activeIndex - 1) % 1;
-  switch(activeIndex) {
-    case 0:
-      activeIndex=1;
-      break;
-    case 1:
-      activeIndex=0;
-      break;
-    default:
-      activeIndex=0;
-  }
-  updateIndicators(activeIndex);
-}
-
-// Scroll Right button
-const scrollRight = function() {
-  let movieWidth = document.querySelector(".movie").getBoundingClientRect()
-    .width;
-  let scrollDistance = movieWidth * 6; // Scroll the length of 6 movies
-
-  // if we're on the last page
-  if (activeIndex == 1) {
-    createSlide("bestMoviesSlider", bestsArray);
-    slider.scrollBy({
-      top: 0,
-      left: +scrollDistance,
-      behavior: "smooth",
-    });
-    activeIndex = 0;
-    updateIndicators(activeIndex);
-  } else {
-    slider.scrollBy({
-      top: 0,
-      left: +scrollDistance,
-      behavior: "smooth",
-    });
-    activeIndex = (activeIndex + 1) % 3;
-    updateIndicators(activeIndex);
-  }
-}
-
-btnRight.addEventListener("click", (e) => {
-  scrollRight();  
-});
-
-btnLeft.addEventListener("click", (e) => {
-  scrollLeft();
-});
-
-
 //MODAL SECTION
-
 //Get movie's detailed informations
 async function getMovieDetails (id) {
   let response = await fetch ("http://127.0.0.1:8000/api/v1/titles/"+id);
@@ -171,13 +181,8 @@ async function getMovieDetails (id) {
   return await data;
 }
 
-// Get the modal
+// Get the modal and close button
 var modal = document.getElementById("modal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("button1");
-
-// Get the <span> element that closes the modal
 var closeButton = document.getElementById("closeButton");
 
 // When the user clicks the button, open the modal 
