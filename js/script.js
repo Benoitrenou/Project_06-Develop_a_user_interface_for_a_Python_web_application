38db5303e6035bbef4aaa892ddb707abd26b3df3
Benoitrenou <benoit.renou@gmail.com>
2021-09-20 09:20:29 -0300

    Update
    HTML update ofr W3 validation

--- js/script.js
+++ js/script.js
@@ -218,13 +218,13 @@ let openModal = async function (movie_id) {
 
 // When the user clicks on (x), close the modal
 closeButton.onclick = function() {
-    modal.style = "display:none"
+    modal.style = "display:none";
   }
 
 //When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-      modal.style = "display:none";
+      modal.style = "disaply:none";
     }
 }
 
